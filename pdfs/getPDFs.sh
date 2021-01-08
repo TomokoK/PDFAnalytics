7bd9438f8e45b04cdc4c1e7ee8c070a502482804
TomokoK <PrivateTracker1941@gmail.com>
2021-01-08 10:46:40 -0500

    more documentation

--- pdfs/getPDFs.sh
+++ pdfs/getPDFs.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Originally went to 1907 as everything after had a different URL for fr/en
-# As of 2020-11=15 the following need to be manually downloaded
+# Some documents need to be designated English (see below)
+# As of 2021-01-08 the following need to be manually downloaded
 # 1912, 1936, 1957, 1958, 1959, 1960, 1961, 1962, 1963
 # Just append "-e" to "-IAAR-RAAI"
 
-#for i in {1864..1907}
+# As of 2021-01-08 the following are missing
+# 1865, 1866, 1867
+
 for i in {1864..1966}
 do
 	wget "http://central.bac-lac.gc.ca/.item/?id="$i"-IAAR-RAAI&op=pdf&app=indianaffairs"
